7b9053c3a1ee0cc57fc5664020a82b21848d1e03
Lehuy <LeHuy.hoang@outlook.com>
2023-05-29 18:14:37 +0000

    feat: add sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center">
+  <div class="min-h-screen flex items-center justify-center gap-x-4">
     <Grid/>
+    <Sidebar/>
   </div>
 </template>
---
+++ components/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+    <aside class="h-full text-center space-y-2">
+        <h1 class="text-lg max-w-xs mx-auto">Use The Image and Hints to Find The Word!</h1>
+        <img src="https://picsum.photos/200" class="rounded mx-auto"/>
+        <details>
+            <summary>💡 Hint 1</summary>
+            This is a hint
+        </details>
+        <details>
+            <summary>💡 Hint 2</summary>
+            This is a hint again!
+        </details>
+    </aside>
+</template>
