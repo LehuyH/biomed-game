9d3281d208a49acc90f8a0bb07d79b090ec143ea
Lehuy <LeHuy.hoang@outlook.com>
2023-05-30 18:35:09 +0000

    feat: add rest of levels

--- composables/state.ts
+++ composables/state.ts
@@ -50,5 +50,56 @@ export const levels:Level[] = [
     },{
         answer:'Centromere',
         hints:['Center of the chromosome','The place where two sister chromatids meet']
+    },{
+        answer:'DNA',
+        hints:['Stores genetic information','Is a double helix shape']
+    },{
+        answer:'Empathy',
+        hints:['Imporant skill for medical doctors during patient interviews','Understand another person\'s perspective and emotions']
+    },{
+        answer:'Fungus',
+        hints:['Living multicellular organism that can cause tissue damage on its hosts','Can lead to athlete\'s foot']
+    },{
+        answer:'Genotype',
+        hints:['Shows a possible allele combination','Example would be AA or Aa']
+    },{
+        answer:'Hypertension',
+        hints:['Common condition leading to high blood pressure','Can be resolved by eating a healthy diet and exercising']
+    },{
+        answer:'Isotonic',
+        hints:['This is the type of solution normal saline is','Pressure is the same on the inside as the outside']
+    },{
+        answer: 'Karyotype',
+        hints:['A chart that shows all chromosomes','Useful for finding trisomies and monosomies']
+    },{
+        answer:'Leukocytes',
+        hints:['An elevated level of this can indicate an infection','Cells that fight off foreign invaders']
+    },{
+        answer:'Monosomy',
+        hints:['Term for missing chromosome','Can lead to conditions like turner syndrome']
+    },{
+        answer:'Nucleus',
+        hints:['Organelle located at the center of the cell','The control center of the cell']
+    },{
+        answer:'Oncology',
+        hints:['One of these rooms was the origin of the hospital breakout at GNMH','The study of cancer/tumors']
+    },{
+        answer:'Plasma',
+        hints:['Makes up the majority of blood','Liqud portion of blood that helps maintain blood pressure, volume, and pH balance']
+    },{
+        answer:'RigorMortis',
+        hints:['Occurs a few hours after death','Leads to stiff joints/muscles']
+    },{
+        answer:'Systemic',
+        hints:['Related to body','A classification for arteries that pump into the body']
+    },{
+        answer:'Transcription',
+        hints:['First step of protein synthesis','Leads to creation of mRNA']
+    },{
+        answer:'Vein',
+        hints:['Carries deoxygenated blood','Blood travels here to reach the lungs for oxygen']
+    },{
+        answer:'Wheel',
+        hints:['Type of crime scene investigation strategy','Begin in the middle of the scene and search outwards']
     }
 ]
